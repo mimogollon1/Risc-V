07bd61fc16e2ba2a01e0f9b8481cb13eee84012c
miguel <mimogollon1@poligran.edu.co>
2019-11-25 16:28:38 -0500

    add the operation

---
+++ Riscj/Assets/Scripts/Contador.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Contador : MonoBehaviour {
+
+	public static void inicia()
+    {
+        Image panelRam = GameObject.Find("ramPanel").GetComponent<Image>();
+        panelRam.color = Color.clear;
+        Image panelPc = GameObject.Find("PCPanel").GetComponent<Image>();
+        panelPc.color = panelPc.color = new Color32(255, 0, 0, 100);
+    }
+}
